391c75a84460fd43911b950980c654c4b5dc1ec6
Marcus Xavier <marcus.xavier@appmax.com.br>
2022-07-26 14:20:09 -0300

    Adicionei a função que valida o input do usuário
    Adicionei a função que valida o input do usuário
    
    Modifiquei a validação

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main() {
-	err := WriteFile("Testando\n", "/tmp/test")
-	check(err)
+	if len(os.Args) < 2 || len(os.Args) > 2 {
+		usage()
+	}
+
 }
 
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %s \"sentence to add\"\n", os.Args[0])
+	flag.PrintDefaults()
+	os.Exit(2)
 }
