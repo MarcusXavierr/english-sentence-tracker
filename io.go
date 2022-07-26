a59d8191a556f54b50337de7ce3121f4bfe9b6a0
Marcus Xavier <marcus.xavier@appmax.com.br>
2022-07-26 16:49:11 -0300

    Corrigi o bug de não adicionar uma linha

--- io.go
+++ io.go
@@ -14,7 +14,7 @@ func ReadFile(filePath string) string {
 func WriteFile(sentence, filepath string) error {
 	file, e := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.FileMode(644))
 	check(e)
-	_, err := file.WriteString(sentence)
+	_, err := file.WriteString(sentence + "\n")
 	check(err)
 	return file.Close()
 }
